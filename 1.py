e60c357b826c295426a7ffe84bdb120d8ee49ada
John S. Rajeski, Ed.D <48130054+rajeski@users.noreply.github.com>
2022-03-22 21:02:23 +0900

    Upload Project-files
    Added Python-file.

---
+++ 1.py
@@ -0,0 +1,14 @@
+#1. Program greetings.
+print("Good day! Welcome to the Band Generator.")
+
+#2. Input hometown.
+city = input("Please tell us your city name. \n")
+
+#3. Input the name of a pet.
+pet = input("Please tell us your pet's name. \n")
+
+#4. Combine the two names together.
+band_name = f"{city} {pet}"
+
+#5. Program results.
+print(f"Your band name should be {band_name}.")
